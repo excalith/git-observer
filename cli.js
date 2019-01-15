8f1264340fbbf46e638d91a5b1e54db5ae3830dc
Can Cellek <can.cellek@gmail.com>
2019-01-16 00:14:22 +0300

    Add JSON Setting Editing Option

--- cli.js
+++ cli.js
@@ -4,24 +4,92 @@ const Configstore = require('configstore')
 const inquirer = require('inquirer')
 const chalk = require('chalk')
 const pkg = require('./package.json')
-
 const conf = new Configstore(pkg.name, {
-	isConfirmed: 'No'
+	currentProject: '',
+	projects: {}
 })
 
-if (conf.get('isConfirmed') == 'No') {
-	AddProject()
-} else {
-	CheckArguments()
+
+// If settings have at least 1 project entry
+if (FetchProjects().length > 0)
+{
+	// If everything is in order
+	if (ValidateString(conf.get('currentProject'))) 
+	{
+		CheckArguments()
+	}
+	// If we do not have current project set
+	else
+	{
+		console.log(chalk.red.bold('\nLast project not found, please select another project\n'))
+		GetProjects()
+	}
 }
+// If settings doesn't have any project entry
+else
+{
+	AddProject()
+}
+
+
+/**
+ * Checks all arguments given from terminal
+ */
+function CheckArguments() {
+	let type = process.argv[2]
+	let command = process.argv[3]
+	let project = conf.get('currentProject')
+
+	if (type == '-i' || type == '--issue') {
+		let issueLink = conf.get('projects.' + project + '.issueLink')
+		OpenLink(issueLink, 'Issue', command)
+	} else if (type == '-c' || type == '--commit') {
+		let commitLink = conf.get('projects.' + project + '.commitLink')
+		OpenLink(commitLink, 'Commit', command)
+	} else if (type == '-p' || type == '--pull') {
+		let prLink = conf.get('projects.' + project + '.pullRequestLink')
+		OpenLink(prLink, 'Pull-Request', command)
+	} else if (type == '-a' || type == '--add') {
+		AddProject()
+	} else if (type == '-g' || type == '--get') {
+		GetProjects()
+	} else if (type == '-e' || type == '--edit') {
+		OpenSettings()
+	} else if (type == '-d' || type == '--delete') {
+		DeleteProject()
+	} else {
+		ShowHelp()
+	}
+}
+
+
+/**
+ * Prints commands into terminal
+ */
+function ShowHelp() {
+	console.log('')
+	console.log(chalk.green.bold('Git Observer') + chalk.magenta.bold(' v' + pkg.version))
+	console.log('')
+	console.log('Commands:')
+	console.log('   -a --add                Add a new project to observer')
+	console.log('   -g --get                Get all projects on observer')
+	console.log('   -e --edit               Edit settings file (JSON)')
+	console.log('   -d --delete             Delete a project from observer')
+	console.log('')
+	console.log('   -i --issue [Number]     Check issue on browser')
+	console.log('   -c --commit  [Hash]     Check commit on browser')
+	console.log('   -p --pull  [Number]     Check pull request on browser')
+	console.log('')
+	console.log('   -h --help               Display this help')
+	console.log('')
+}
+
 
 /**
  * Adds a project to store in settings and activates it
- *
  */
 function AddProject() {
-	console.log(chalk.white.bold('\nSettings will ask for links below.'))
-	console.log('')
+	console.log(chalk.white.bold('\nSettings will ask for links below.\n'))
 	console.log(
 		chalk.white.bold('Project Commits Example: ') +
 		'https://github.com/User/RepoName/commit/')
@@ -30,33 +98,32 @@ function AddProject() {
 		'https://github.com/User/RepoName/issues/')
 	console.log(
 		chalk.white.bold('Project Issues Example: ') +
-		'https://github.com/User/RepoName/pull/')
-	console.log('')
+		'https://github.com/User/RepoName/pull/\n')
 	inquirer
 		.prompt([
 			{
 				message: 'Project Name:',
 				type: 'input',
 				name: 'projectName',
-				validate: validateString
+				validate: ValidateString
 			},
 			{
 				message: 'Project Commits:',
 				type: 'input',
 				name: 'commitLink',
-				validate: validateLink
+				validate: ValidateLink
 			},
 			{
 				message: 'Project Issues:',
 				type: 'input',
 				name: 'issueLink',
-				validate: validateLink
+				validate: ValidateLink
 			},
 			{
 				message: 'Project Pull-Requests:',
 				type: 'input',
 				name: 'pullRequestLink',
-				validate: validateLink
+				validate: ValidateLink
 			},
 			{
 				message: 'Are your choices correct?',
@@ -69,8 +136,8 @@ function AddProject() {
 			if (answers.isConfirmed == 'No') {
 				AddProject()
 			} else {
-				conf.set('isConfirmed', true)
-				conf.set('lastProject', answers.projectName)
+				// conf.set('isConfirmed', true)
+				conf.set('currentProject', answers.projectName)
 
 				conf.set(
 					'projects.' + answers.projectName + '.commitLink',
@@ -85,19 +152,19 @@ function AddProject() {
 					answers.pullRequestLink
 				)
 
-				console.log('')
-				console.log('You have created project ' + chalk.green(answers.projectName))
-				console.log('')
+				console.log('\nYou have created project ' + chalk.green(answers.projectName) + '\n')
 			}
 		})
 }
 
+
 /**
  * Shows all projects stored in settings and activates selected
- *
  */
 function GetProjects() {
-	let projects = fetchProjects()
+	let projects = FetchProjects()
+	projects.push(new inquirer.Separator())
+	projects.push('Exit')
 
 	inquirer
 		.prompt([
@@ -110,23 +177,30 @@ function GetProjects() {
 		])
 		.then(answers => {
 			if (answers.projectName == 'Exit') {
-				console.log('You didn\'t select a project')
-				console.log('')
+				console.log('\nYou didn\'t select a project\n')
 			} else {
-				conf.set('lastProject', answers.projectName)
-				console.log('You have selected ' + chalk.green(answers.projectName))
-				console.log('')
+				conf.set('currentProject', answers.projectName)
+				console.log('\nYou have selected ' + chalk.green(answers.projectName) + '\n')
 			}
 		})
 }
 
+
+/**
+ * Open configstore settings file
+ **/
+function OpenSettings() {
+	opn(conf.path)
+}
+
+
 /**
  * Shows all projects stored in settings and deletes selected
- *
  */
 function DeleteProject() {
-	let projects = fetchProjects()
-
+	let projects = FetchProjects()
+	projects.push(new inquirer.Separator())
+	projects.push('Exit')
 	inquirer
 		.prompt([
 			{
@@ -138,118 +212,67 @@ function DeleteProject() {
 		])
 		.then(answers => {
 			if (answers.projectName == 'Exit') {
-				console.log('You didn\'t delete any project')
-				console.log('')
+				console.log('You didn\'t delete any project\n')
 			} else {
 				conf.delete('projects.' + answers.projectName)
 				console.log(
 					'You have deleted ' +
 					chalk.red(answers.projectName) +
-					' from settings')
-				console.log('')
+					' from settings\n')
 			}
 		})
 }
 
-/**
- * Prints commands into terminal
- *
- */
-function ShowHelp() {
-	console.log('')
-	console.log(chalk.white.bold('Git Observer v' + pkg.version))
-	console.log('')
-	console.log('Commands:')
-	console.log('   -a --add                Add a new project to observer')
-	console.log('   -g --get                Get all projects on observer')
-	console.log('   -d --delete             Delete a project from observer')
-	console.log('')
-	console.log('   -i --issue [Number]     Check issue on browser')
-	console.log('   -c --commit [Hash]      Check commit on browser')
-	console.log('   -p --pull [Number]      Check pull request on browser')
-	console.log('')
-	console.log('   -h --help               Display this help')
-	console.log('')
-}
-
-/**
- * Checks all arguments given from terminal
- *
- */
-function CheckArguments() {
-	let type = process.argv[2]
-	let command = process.argv[3]
-	let project = conf.get('lastProject')
-
-	if (type == '-i' || type == '--issue') {
-		let issueLink = conf.get('projects.' + project + '.issueLink')
-		openLink(issueLink, 'Issue', command)
-	} else if (type == '-c' || type == '--commit') {
-		let commitLink = conf.get('projects.' + project + '.commitLink')
-		openLink(commitLink, 'Commit', command)
-	} else if (type == '-p' || type == '--pull') {
-		let prLink = conf.get('projects.' + project + '.pullRequestLink')
-		openLink(prLink, 'Pull-Request', command)
-	} else if (type == '-a' || type == '--add') {
-		AddProject()
-	} else if (type == '-g' || type == '--get') {
-		GetProjects()
-	} else if (type == '-d' || type == '--delete') {
-		DeleteProject()
-	} else {
-		ShowHelp()
-	}
-}
 
 /**
  * If link is stored in settings, launch on browser. Else display error
- *
+ * @param  {String} link Link to open
+ * @param  {String} linkType Issue, Commit or Pull Request
+ * @param  {String} id ID of link
  */
-function openLink(link, linkType, command) {
+function OpenLink(link, linkType, id) {
 	if (link === '') {
-		console.log(linkType + ' link not set for project ' + chalk.magenta.bold(conf.get('lastProject')))
+		console.log(linkType + ' link not set for project ' + chalk.magenta.bold(conf.get('currentProject')))
 	} else {
 		if (linkType == 'Issue')
-			console.log('Opening ' + chalk.red.bold('issue #' + command) + ' of project ' + chalk.magenta.bold(conf.get('lastProject')))
+			console.log('Opening ' + chalk.red.bold('issue #' + id) + ' of project ' + chalk.magenta.bold(conf.get('currentProject')))
 		else if (linkType == 'Commit')
-			console.log('Opening ' + chalk.green.bold('commit #' + command) + ' of project ' + chalk.magenta.bold(conf.get('lastProject')))
+			console.log('Opening ' + chalk.green.bold('commit #' + id) + ' of project ' + chalk.magenta.bold(conf.get('currentProject')))
 		else
-			console.log('Opening ' + chalk.blue.bold('pull request #' + command) + ' of project ' + chalk.magenta.bold(conf.get('lastProject')))
-		opn(link + command)
+			console.log('Opening ' + chalk.blue.bold('pull request #' + id) + ' of project ' + chalk.magenta.bold(conf.get('currentProject')))
+
+		opn(link + id)
 	}
 }
 
+
 /**
  * Fetch all projects stored in Git-Observer settings
- *
  */
-function fetchProjects() {
+function FetchProjects() {
 	let projects = []
 
 	Object.keys(conf.get('projects')).forEach(element => {
 		projects.push(element)
 	})
 
-	projects.push('Exit')
-
 	return projects
 }
 
+
 /**
  * Validates string
- *
  * @param  {String} str String to validate
  */
-function validateString(str) {
+function ValidateString(str) {
 	return str !== ''
 }
 
 /**
  * Validates link
- *
- * @param  {String} str String to validate
+ * @param  {String} str Link to validate
  */
-function validateLink(str) {
+function ValidateLink(str) {
 	const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
 
 	if (str === '') return true
